d7c5d1b819d2e92518e06b69bddc07ded0646025
Nasser Ramadan <nasserramadan22@gmail.com>
2023-12-04 16:16:08 -0300

    Alterações nos códigos de HTML, CSS e JS

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
 const form = document.getElementById('form-cadastro');
-
+let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-});
 
-
-let linhas = '';
-
-
-adicionaLinha();
-atualizaTabela();
-
-
-function adicionaLinha() {
     const inputNomeCadastro = document.getElementById('nome-cadastro');
     const inputTelefoneCadastro = document.getElementById('telefone-cadastro');
-
 
     let linha = '<tr>';
     linha += `<td>${inputNomeCadastro.value}</td>`;
     linha += `<td>${inputTelefoneCadastro.value}</td>`;
     linha += '</tr>';
 
-
     linhas += linha;
 
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
 
     inputNomeCadastro.value = '';
     inputTelefoneCadastro.value = '';
-}
-
-
-function atualizaTabela() {
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-}
+});
